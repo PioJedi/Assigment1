7bfb6b53d60f99e2c7172123bec8c840b5f91851
PioJedi <81320805+PioJedi@users.noreply.github.com>
2021-03-26 12:38:28 -0700

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,7 @@
+Author: "Mario_Navarro"
+Date: 3/23/2021
+
+Description: ("Ask  the user for their favorite animal and then print out that that is their favorite animal")
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
